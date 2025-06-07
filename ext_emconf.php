6021565181955ad46674cc2b4f8e9e6c365b5efd
Sven Wappler <wappler@wappler.systems>
2025-06-08 01:17:07 +0200

    first commit

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Frontend Quicklinks',
+    'description' => '',
+    'category' => 'fe',
+    'state' => 'alpha',
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3@wappler.systems',
+    'author_company' => 'WapplerSystems',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
